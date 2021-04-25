9d283d252d490a0995853eee6f557e176bdb5119
hengyuni <hengyuni@gmail.com>
2021-04-25 13:48:37 -0400

    drink drank drunk

--- hipyelp/models.py
+++ hipyelp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Ftag(models.Model):
+  tags = models.CharField(max_length=20)
+
 class Food(models.Model):
   name = models.CharField(max_length=100)
   group = models.CharField(max_length=100, default='groups')
@@ -8,7 +11,14 @@ class Food(models.Model):
   photo_url = models.CharField(max_length=200, null=True)
   lon = models.CharField(max_length=20, default='lon')
   lat = models.CharField(max_length=20, default='lat')
+  tags = models.ManyToManyField(Ftag, related_name='foods',blank=True)
 
+class FoodTag(models.Model):
+  test = models.CharField(max_length=100, default='')
+  tags = models.ForeignKey(Food, related_name='foods', on_delete=models.CASCADE, default='')
+
+class Dtag(models.Model):
+  tags = models.CharField(max_length=20)
 
 class Drink(models.Model):
   name = models.CharField(max_length=100)
@@ -18,19 +28,18 @@ class Drink(models.Model):
   photo_url = models.CharField(max_length=200, null=True)
   lon = models.CharField(max_length=20, default='lon')
   lat = models.CharField(max_length=20, default='lat')
-
-class FoodTag(models.Model):
-  foodName = models.ManyToManyField(
-    Food,
-    related_name='foodTags',
-    related_query_name='foodtag',
-  )
-  tagName = models.TextField()
+  tags = models.ManyToManyField(Ftag, related_name='drinks',blank=True)
+  # tags = models.CharField(DrinkTag, max_length=50)
 
 class DrinkTag(models.Model):
-  drinkName = models.ManyToManyField(
-    Drink,
-    related_name='drinkTags',
-    related_query_name=' drinktag',
-  )
-  tagName = models.TextField()
+  test = models.CharField(max_length=100, default='')
+  tags = models.ForeignKey(Drink, related_name='drinks', on_delete=models.CASCADE, default='')
+
+
+# class DrinkTag(models.Model):
+#   drinkName = models.ManyToManyField(
+#     Drink,
+#     related_name='drinkTags',
+#     related_query_name=' drinktag',
+#   )
+#   tagName = models.TextField()
--- hipyelp/serializers.py
+++ hipyelp/serializers.py
@@ -1,30 +1,47 @@
-from .models import Food, Drink, FoodTag, DrinkTag
+from .models import Food, Drink, FoodTag, DrinkTag, Ftag, Dtag
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+class FtagSerializer(serializers.HyperlinkedModelSerializer):
+
+  class Meta:
+    model = Ftag
+    fields = '__all__'
 # FoodSerializer
 class FoodSerializer(serializers.HyperlinkedModelSerializer):
-  foodTags = serializers.SlugRelatedField(many=True, slug_field='tagname', queryset=FoodTag.objects.all())
+  # foodTags = serializers.SlugRelatedField(many=True, slug_field='tagName', queryset=FoodTag.objects.all())
   class Meta:
     model = Food
-    fields = ['id', 'name', 'group', 'address', 'description', 'photo_url', 'lon', 'lat', 'foodTags']
+    fields = '__all__'
 
 # FoodTagSerializer
 class FoodTagsSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = FoodTag
-    fields = ('tagname')
+    fields = '__all__'
+
+class PopulatedFoodTagSerializer(serializers.HyperlinkedModelSerializer):
+  food = FoodSerializer()
+  tags = FtagSerializer(many=True)
+
+class DtagSerializer(serializers.HyperlinkedModelSerializer):
+
+  class Meta:
+    model = Dtag
+    fields = '__all__'
 
 # DrinkSerializer
 class DrinkSerializer(serializers.HyperlinkedModelSerializer):
-  drinkTags = serializers.SlugRelatedField(many=True, slug_field='tagname', queryset=DrinkTag.objects.all())
+  # drinkTags = serializers.SlugRelatedField(many=True, slug_field='tagName', queryset=DrinkTag.objects.all())
   class Meta:
     model = Drink
-    fields = ['id', 'name', 'group', 'address', 'description', 'photo_url', 'lon', 'lat', 'drinkTags']
+    fields = '__all__'
 
 # DrinkTagsSerializer
 class DrinkTagsSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = DrinkTag
-    fields = ('tagname')
+    fields = '__all__'
 
+class PopulatedDrinkTagSerializer(serializers.HyperlinkedModelSerializer):
+  drink = DrinkSerializer()
